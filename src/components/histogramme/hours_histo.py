7e4cbd5c820e68a2dec3988cf9d0077b672b0daf
Noah Guerreiro <noah.guerreiro1801@gmail.com>
2026-01-24 12:20:37 +0100

    ajout de histo pour les ages

--- src/components/histogramme/hours_histo.py
+++ src/components/histogramme/hours_histo.py
@@ -3,29 +3,22 @@ from __future__ import annotations
 import pandas as pd
 import plotly.express as px
 
-from src.utils.clean_data import load_cleaned_range
-
-def generate_hours_histogram(
-    start_year: int = 2005,
-    end_year: int = 2024,
-):
+def generate_hours_histogram(df: pd.DataFrame):
     """
     Retourne une figure Plotly pour être affichée dans Dash via dcc.Graph.
     """
-    df: pd.DataFrame = load_cleaned_range(start_year, end_year)
-
     if "heure" not in df.columns:
         raise KeyError("La colonne 'heure' n'existe pas dans les données nettoyées.")
 
     counts = df["heure"].dropna().value_counts().sort_index()
     if counts.empty:
-        return px.bar(title=f"Aucune donnée d'heure entre {start_year} et {end_year}.")
+        return px.bar(title=f"Aucune donnée d'heure entre 2005 et 2024.")
 
     fig = px.bar(
         x=counts.index.astype(str),
         y=counts.values,
         labels={"x": "Heures de la journée", "y": "Nombre d'accidents"},
-        title=f"Répartition des accidents selon l'heure ({start_year}-{end_year})",
+        title=f"Répartition des accidents selon l'heure (2005-2024)",
     )
     fig.update_layout(xaxis_tickangle=30)
     return fig
--- src/pages/home.py
+++ src/pages/home.py
@@ -4,33 +4,44 @@ from dash import html, dcc
 from src.components.histogramme.lumin_histo import generate_luminosite_histogram
 from src.components.histogramme.meteo_histo import generate_meteo_histogram
 from src.components.histogramme.hours_histo import generate_hours_histogram
+from src.components.histogramme.age_histo import generate_age_histogram
+from src.utils.clean_data import load_cleaned_range
 
 dash.register_page(__name__, path="/", name="Accueil")
 
 def layout():
-    fig = generate_luminosite_histogram(2005, 2024)
-    fig2 = generate_meteo_histogram(2005, 2024)
-    fig3 = generate_hours_histogram(2005, 2024)
+    datas = load_cleaned_range(2005, 2024)
+    histo = generate_luminosite_histogram(datas)
+    histo2 = generate_meteo_histogram(datas)
+    histo3 = generate_hours_histogram(datas)
+    histo4 = generate_age_histogram(datas)
     return html.Div([
         html.H1("Dashboard des accidents"),
         html.Div(
             [
                 html.H2("Histogramme de la luminosité"),
-                dcc.Graph(figure=fig),
+                dcc.Graph(figure=histo),
             ],
             style={"padding": "16px"}
         ),
         html.Div(
             [
                 html.H2("Histogramme de la météo"),
-                dcc.Graph(figure=fig2),
+                dcc.Graph(figure=histo2),
             ],
             style={"padding": "16px"}
         ),
         html.Div(
             [
                 html.H2("Histogramme des heures de la journée"),
-                dcc.Graph(figure=fig3),
+                dcc.Graph(figure=histo3),
+            ],
+            style={"padding": "16px"}
+        ),
+        html.Div(
+            [
+                html.H2("Histogramme des ages des conducteurs qui ont fait un accident"),
+                dcc.Graph(figure=histo4),
             ],
             style={"padding": "16px"}
         ),
